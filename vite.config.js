572bea60450e891dbf423b4e57c7b474bb7a0297
jonnyboi42 <connelljon9@gmail.com>
2024-10-30 19:48:33 -0500

    Vite Config Changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // Ensure this matches your GitHub repo name
 })
